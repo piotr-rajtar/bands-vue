964f32b9df8dc4c6b840b052298cb8238cb34d46
Piotr Rajtar <piotrek.rajtar@gmail.com>
2021-02-17 18:09:55 +0100

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header />
+  <dynamic-list />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/Layout/TheHeader.vue';
+import DynamicList from './components/DynamicList.vue';
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    TheHeader,
+    DynamicList,
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
